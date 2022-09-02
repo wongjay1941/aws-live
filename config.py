424588f36fe607a0faa01afca4ca55001ce820f5
wongjay1941 <73803916+wongjay1941@users.noreply.github.com>
2022-09-02 16:24:19 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.cmvprgseuylq.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "wongwaizhong-employee"
+customregion = "us-east-1"
